19e7c204848442499e919371f6e6fc8ef362ab86
Wellington Lopes de Souza <wellingtonsouza32@hotmail.com>
2023-07-12 11:46:21 -0300

    Update 3_visao_restaurantes.py
    datetime

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -7,6 +7,7 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 import folium
+from datetime import datetime
 from streamlit_folium import folium_static
 import plotly.graph_objects as go
 
@@ -160,9 +161,9 @@ st.sidebar.markdown("""___""")
 st.sidebar.markdown('## Selecione uma data limite')
 
 date_slider = st.sidebar.slider('Até qual valor?',
-                  value=pd.datetime(2022,4,13),
-                  min_value=pd.datetime(2022,2,11),
-                  max_value=pd.datetime(2022,4,6),
+                  value=datetime(2022,4,13),
+                  min_value=datetime(2022,2,11),
+                  max_value=datetime(2022,4,6),
                   format='DD-MM-YYYY')
 
 st.sidebar.markdown("""___""")
