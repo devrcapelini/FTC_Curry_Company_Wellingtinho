c9b6b3159ec9e03ec4dfdaa16594730a6e5d3c00
Wellington Lopes de Souza <wellingtonsouza32@hotmail.com>
2023-07-12 10:15:06 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -1,6 +1,5 @@
 # Libraries
 import pandas as pd
-from pandas import datetime
 import plotly.express as px
 import re
 from haversine import haversine
@@ -158,9 +157,9 @@ st.sidebar.markdown("""___""")
 st.sidebar.markdown('## Selecione uma data limite')
 
 date_slider = st.sidebar.slider('Até qual valor?',
-                  value=pd.datetime(2022,4,13),
-                  min_value=pd.datetime(2022,2,11),
-                  max_value=pd.datetime(2022,4,6),
+                  value=pd.to_datetime(2022,4,13),
+                  min_value=pd.to_datetime(2022,2,11),
+                  max_value=pd.to_datetime(2022,4,6),
                   format='DD-MM-YYYY')
 
 st.sidebar.markdown("""___""")
