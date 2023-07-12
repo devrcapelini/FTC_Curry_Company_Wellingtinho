6b72ffcec089120017b0695ef2afa1ad90437740
Wellington Lopes de Souza <wellingtonsouza32@hotmail.com>
2023-07-12 11:37:56 -0300

    Update 1_visao_empresa.py
    import datetime

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -6,6 +6,7 @@ from haversine import haversine
 import streamlit as st
 from PIL import Image
 import folium
+from datetime import datetime
 from streamlit_folium import folium_static
 
 st.set_page_config(page_title='Visão Empresa',layout='wide')
@@ -157,9 +158,9 @@ st.sidebar.markdown("""___""")
 st.sidebar.markdown('## Selecione uma data limite')
 
 date_slider = st.sidebar.slider('Até qual valor?',
-                  value=pd.datetime(2022,4,13),
-                  min_value=pd.datetime(2022,2,11),
-                  max_value=pd.datetime(2022,4,6),
+                  value=datetime(2022,4,13),
+                  min_value=datetime(2022,2,11),
+                  max_value=datetime(2022,4,6),
                   format='DD-MM-YYYY')
 
 st.sidebar.markdown("""___""")
